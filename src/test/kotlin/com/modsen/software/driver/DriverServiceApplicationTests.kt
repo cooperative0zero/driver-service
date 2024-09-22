65c3d68e49e74cdcc210f6dc26680feb0ba66685
aleksej <man747704@gmail.com>
2024-09-22 13:24:58 +0300

    feat: initialize new project

---
+++ src/test/kotlin/com/modsen/software/driver/DriverServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.modsen.software.driver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DriverServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
